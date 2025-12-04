beece197edab379dbe80a82aa98fb7ab72c56f66
chiba <motoki_chiba@jp.honda>
2025-12-04 15:40:33 +0000

    python node class

--- ros2_ws/build/my_py_pkg/build/lib/my_py_pkg/my_first_node.py
+++ ros2_ws/build/my_py_pkg/build/lib/my_py_pkg/my_first_node.py
@@ -2,10 +2,19 @@
 import rclpy
 from rclpy.node import Node
 
+class MyNode(Node):
+
+    def __init__(self):
+        super().__init__("py_test")
+        self.get_logger().info("Hello world")
+        self.create_timer(1.0, self.timer_callback)
+
+    def timer_callback(self):
+        self.get_logger().info("Hello")
+
 def main(args=None):
     rclpy.init(args=args)
-    node = Node("py_test")
-    node.get_logger().info("Hello world")
+    node = MyNode()
     rclpy.spin(node)
     rclpy.shutdown()
 
