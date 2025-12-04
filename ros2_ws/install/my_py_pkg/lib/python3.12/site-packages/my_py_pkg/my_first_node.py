9e5781eba6797f372e9614dc0053be09aeb6db5b
chibamo <dulcet71@gmail.com>
2025-12-05 07:56:06 +0900

    cpp minimal code

--- ros2_ws/install/my_py_pkg/lib/python3.12/site-packages/my_py_pkg/my_first_node.py
+++ ros2_ws/install/my_py_pkg/lib/python3.12/site-packages/my_py_pkg/my_first_node.py
@@ -6,11 +6,13 @@ class MyNode(Node):
 
     def __init__(self):
         super().__init__("py_test")
+        self.counter_ = 0
         self.get_logger().info("Hello world")
         self.create_timer(1.0, self.timer_callback)
 
     def timer_callback(self):
-        self.get_logger().info("Hello")
+        self.get_logger().info("Hello" + str(self.counter_))
+        self.counter_ += 1
 
 def main(args=None):
     rclpy.init(args=args)
